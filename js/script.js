33d056f2920afa71c45c67edcb36c1ad4f7d5a70
Rajendra <kumarrajendra8802@gmail.com>
2022-08-31 17:58:41 +0530

    bootstrapOffline

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// var myNav = document.getElementById('mynav');
+// window.onscroll = function () { 
+//     "use strict";
+//     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
+//         myNav.classList.add("nav-colored");
+//         myNav.classList.remove("nav-transparent");
+//     } 
+//     else {
+//         myNav.classList.add("nav-transparent");
+//         myNav.classList.remove("nav-colored");
+//     }
+// };
+
