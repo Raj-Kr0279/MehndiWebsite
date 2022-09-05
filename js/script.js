c300e74091b5e9bc5de564b894459696397699f6
Rajendra <kumarrajendra8802@gmail.com>
2022-09-05 23:52:15 +0530

    final-changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
-// var myNav = document.getElementById('mynav');
-// window.onscroll = function () { 
-//     "use strict";
-//     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
-//         myNav.classList.add("nav-colored");
-//         myNav.classList.remove("nav-transparent");
-//     } 
-//     else {
-//         myNav.classList.add("nav-transparent");
-//         myNav.classList.remove("nav-colored");
-//     }
-// };
+// const closeIc = document.querySelector(".fa-xmark");
+const video = document.querySelectorAll("video");
+const x = document.querySelector(".fa-xmark");
 
+video.forEach((play) =>
+  play.addEventListener("click", () => {
+    play.classList.add("active");
+    // play.setAttribute("controls", "true");
+    x.style.visibility = "visible";
+    if (play.paused) {
+      play.play();
+    } else {
+      play.pause();
+      play.currentTime = 0;
+    }
+  })
+);
+function CloseVid() {
+  // const vid = document.querySelectorAll("video");
+
+  console.log("clicked");
+  if (Array.from(video).some(({ classList }) => classList.contains("active"))) {
+    video.forEach((play) => {
+      x.style.visibility = "visible";
+      x.addEventListener("click", () => {
+        // console.log("clicked");
+        x.style.visibility = "hidden";
+        play.classList.remove("active");
+        play.pause();
+        // play.setAttribute("controls", "false");
+      });
+    });
+  }
+}
+
+//   console.log(x);
+//   x.style.visibility = "visible";
+//   play.classList.remove("active");
+// });
